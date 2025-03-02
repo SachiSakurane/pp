30cdc5e8f9a2df0d45fd413bfbd4eb106f11b8ed
Sachi Sakurane <sakuranesachi@gmail.com>
2025-03-02 17:56:30 +0900

    add pp.hpp

---
+++ include/pp/pp.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <pp/accessor.hpp>
+#include <pp/adapter.hpp>
+#include <pp/behavior.hpp>
+#include <pp/concepts.hpp>
+#include <pp/observer.hpp>
+#include <pp/storage.hpp>
+#include <pp/store.hpp>
+#include <pp/subject.hpp>
+#include <pp/subscription.hpp>
