781e8e0ee67b48ac07c3a782b4103f570abcbdaa
Sachi Sakurane <sakuranesachi@gmail.com>
2025-03-07 00:56:57 +0900

    add subject tests

--- test/subject.cpp
+++ test/subject.cpp
@@ -13,3 +13,24 @@ TEST(SubjectTest, Construction) {
   pp::subject<std::string> s;
   pp::subject<ClassA> c;
 }
+
+TEST(SubjectTest, Next) {
+  pp::subject<int> i;
+  i.next(1);
+  i = 32;
+}
+
+TEST(SubjectTest, Subscribe) {
+  pp::subject<int> i;
+
+  testing::internal::CaptureStdout();
+  std::cout << std::endl;
+
+  auto s = i.subscribe([](auto i) { std::cout << "int: " << i << std::endl; });
+  i = 100;
+  std::string log = testing::internal::GetCapturedStdout();
+
+  ASSERT_EQ(static_cast<std::string>(log), R"(
+int: 100
+)");
+}
